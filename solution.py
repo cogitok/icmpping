7d851be7c37c5a72be78d65630c457799d05319d
Jon Henderson <49729580+cogitok@users.noreply.github.com>
2020-11-15 11:51:49 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -103,19 +103,22 @@ def doOnePing(destAddr, timeout):
 	return delay
 	
 def ping(host, timeout=1):
-    def ping(host, timeout=1):
-    #timeout=1 means: If one second goes by without a reply from the server,
-    dest = socket.gethostbyname(host)
-    print "Pinging " + dest + " using Python:"
-    print ""
-    #Send ping requests to a server separated by approximately one second
-    while 1 :
-        delay = doOnePing(dest, timeout)
-        print "RTT:",delay
-        print "maxRTT:", (max(timeRTT) if len(timeRTT) > 0 else 0), "\tminRTT:", (min(timeRTT) if len(timeRTT) > 0 else 0), "\naverageRTT:", float(sum(timeRTT)/len(timeRTT) if len(timeRTT) > 0 else float("nan"))
-        print "Package Lose Rate:", ((packageSent - packageRev)/packageSent if packageRev > 0 else 0)
-        time.sleep(1)# one second
-    return delay
+      # timeout=1 means: If one second goes by without a reply from the server,      # the client assumes that either the client's ping or the server's pong is lost
+   dest = gethostbyname(host)
+   print("Pinging " + dest + " using Python:")
+   print("")
+   # Calculate vars values and return them
+   #  vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
+   # Send ping requests to a server separated by approximately one second
+   for i in range(0,4):
+       delay = doOnePing(dest, timeout)
+       print(delay)
+       print(vars)
+       time.sleep(1)  # one second
+
+   return vars
+
+
 	
 if __name__ == '__main__':
     ping ("google.co.il")
