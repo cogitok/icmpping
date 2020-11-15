e1ca85cebaca481df105e875a65bafdff2e33fb4
Jon Henderson <49729580+cogitok@users.noreply.github.com>
2020-11-14 19:35:59 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -113,7 +113,7 @@ def ping(host, timeout=1):
 		delay = doOnePing(dest, timeout)
 		print(delay)
 		time.sleep(1)# one second
-	#return vars
+	return delay
 	
 if __name__ == '__main__':
     ping ("google.co.il")
